b0a36d8ce0d54a4f5a8dbb45f4e6dc35755c56bd
Joan Bosch <jbosch@adiquimica.com>
2023-02-25 21:16:03 +0100

    keep implementing authentication

--- app/[lang]/login/page.tsx
+++ app/[lang]/login/page.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link'
+import LoginComponent from '../../../components/login'
 
-export default function Page({ params: { lang } } : { params: { lang: string } }) {
+export default function Login({ params: { lang } } : { params: { lang: string } }) {
+
   return (
     <>
-      <h1>Hi from second page!</h1>
+      <h1>Hi from login page!</h1>
+      <LoginComponent params={{
+        lang: lang
+      }} />
       <Link href={`/${lang}`}>
         back
       </Link>
---
+++ components/login.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { useState } from "react"
+import axios from "axios"
+
+export default function LoginComponent({ params: { lang } } : { params: { lang: string } }) {
+
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: ''
+  })
+  
+  const handleChange = (e: { target: { name: any; value: any } }) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value
+    })
+  } 
+  
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault()
+    const response = await axios.post('/api/auth/login', credentials)
+    console.log(response)
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder='email' onChange={handleChange}/>
+        <input type="password" name="password" placeholder='password' onChange={handleChange}/>
+        <button type="submit">Login</button>
+      </form>
+    </>
+  )
+}
