65fcd2d57a5b2ccfa7f770017a7dd569423de8c7
Joan Bosch <jbosch@adiquimica.com>
2023-02-26 18:48:05 +0100

    Beauty landing page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -10,16 +10,18 @@ export default async function Home({ params: { lang } } : { params: { lang: stri
   const { t } = await useTranslation(lang, 'translation')
   return (
     <>
-    <div className="hero container max-w-screen-lg mx-auto pb-10">
-      <Image src="/SOUTH_logo.png" className="mx-auto pt-10" alt="South Original Picture" width={128} height={128}/>
+    <div className="container max-w-screen-lg mx-auto pb-10">
+      <Image src="/SOUTH_logo.png" className="mx-auto pt-10" alt="South Original Picture" width={90} height={64}/>
     </div>
 
     <h1 className="text-center mb-4 mt-4 text-3xl font-extrabold text-gray-900 md:text-5xl lg:text-6xl">{t('title')}</h1>
     <h1 className="text-center mb-4 mt-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">{t('southoriginal')}</span></h1>
 
-    <div className=" ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-orange-400 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-orange-500">
-      <Link href={`/${lang}/login`}>
-        Login
+    <div className="grid place-items-center mt-80">
+      <Link  href={`/${lang}/login`}>
+        <div className="flex justify-center font-bold whitespace-nowrap rounded-md border border-transparent bg-emerald-400 px-4 py-2 text-base text-white shadow-sm hover:bg-emerald-500">
+              {t('login')}
+        </div>
       </Link>
     </div>
 
