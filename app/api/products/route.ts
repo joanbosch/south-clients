9fd56250f34636ca6b34edc3fde7f84d0c9e6d3c
Joan Bosch <jbosch@adiquimica.com>
2023-03-14 20:32:13 +0100

    Add product endpoint

---
+++ app/api/products/route.ts
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+import { cookies } from 'next/headers'
+import executeQuery from '../../../utils/executeQuery'
+
+export async function GET (request: Request) {
+  try {
+    const cookieStore = cookies()
+    const token = cookieStore.get(process.env.TOKEN_NAME || '')
+    const authToken = token?.value
+
+    if (authToken === undefined) {
+      return new Response('No token', { status: 401 })
+    }
+
+    jwt.verify(authToken, process.env.TOKEN_SECRET || '')
+
+    const results: any = await executeQuery({
+      query: "SELECT id as 'id', name as 'name', price as 'price', img1 as 'img1', img2 as 'img2', img3 as 'img3', img4 as 'img4' FROM evtybqup_southclientes.products;"
+    })
+
+    const respone = new Response(JSON.stringify(results))
+    respone.headers.set('Content-Type', 'application/json')
+    return respone
+  } catch (e) {
+    return new Response('Unauthorized', { status: 401 })
+  }
+}
