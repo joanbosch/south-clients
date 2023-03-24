dfa0c83b7125853702b713173f76d030f42920eb
Joan Bosch <jbosch@adiquimica.com>
2023-03-24 19:15:21 +0100

    Keep Working on Cart

--- components/Headers.tsx
+++ components/Headers.tsx
@@ -5,6 +5,7 @@ const Headers = () => {
   return (
     <header className='bg-gradient-to-b from-orange-400 to-orange-200 border shadow p-2 flex'>
       <Link className='text-black' href='/es/dashboard'> Dashboard </Link>
+      <Link className='ml-auto mr-4 ' href='/es/cart'> Carrito </Link>
     </header>
   )
 }
