45a2c50edaf1d1cece2ddec8adadeabb2d259cca
Joan Bosch <jbosch@adiquimica.com>
2023-03-28 20:35:47 +0200

    Fix dependency

--- app/api/accessories/sizes/route.ts
+++ app/api/accessories/sizes/route.ts
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken'
 import { cookies } from 'next/headers'
-import executeQuery from '../../../utils/executeQuery'
+import executeQuery from '../../../../utils/executeQuery'
 
 export async function GET (request: Request) {
   try {
