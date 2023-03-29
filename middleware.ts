8ae396ac9e17498fc3b7b3264c81f2e0b2485a42
Joan Bosch <jbosch@adiquimica.com>
2023-03-29 12:09:08 +0200

    Log token

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,17 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-// import { jwtVerify } from 'jose'
+import { jwtVerify } from 'jose'
 
 export async function middleware (request: NextRequest) {
   const authToken = request.cookies.get(process.env.TOKEN_NAME || '')?.value
+  console.log(authToken)
 
   if (authToken === undefined) {
     return NextResponse.redirect(new URL('/es/login', request.url))
   }
 
   try {
-    // await jwtVerify(authToken, new TextEncoder().encode(process.env.TOKEN_SECRET))
+    await jwtVerify(authToken, new TextEncoder().encode(process.env.TOKEN_SECRET))
     return NextResponse.next()
   } catch (e) {
     return NextResponse.redirect(new URL('/es/login', request.url))
