f942c3b0bec9fb235896518d56ff7e89994ac5cc
Joan Bosch <jbosch@adiquimica.com>
2023-03-29 12:06:18 +0200

    Disable Jwt verification

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { jwtVerify } from 'jose'
+// import { jwtVerify } from 'jose'
 
 export async function middleware (request: NextRequest) {
   const authToken = request.cookies.get(process.env.TOKEN_NAME || '')?.value
@@ -10,7 +10,7 @@ export async function middleware (request: NextRequest) {
   }
 
   try {
-    await jwtVerify(authToken, new TextEncoder().encode(process.env.TOKEN_SECRET))
+    // await jwtVerify(authToken, new TextEncoder().encode(process.env.TOKEN_SECRET))
     return NextResponse.next()
   } catch (e) {
     return NextResponse.redirect(new URL('/es/login', request.url))
