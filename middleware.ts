588af55bf91c3989cca7d49f377e1929c1e3eb12
Joan Bosch <jbosch@adiquimica.com>
2023-03-30 16:49:47 +0200

    fix middle

--- middleware.ts
+++ middleware.ts
@@ -3,18 +3,21 @@ import type { NextRequest } from 'next/server'
 import { jwtVerify } from 'jose'
 
 export async function middleware (request: NextRequest) {
-  const authToken = request.cookies.get(process.env.TOKEN_NAME || '')
+  console.log('middleware')
+  const authToken = request.cookies.get(process.env.TOKEN_NAME || '')?.value
 
   if (authToken === undefined) {
+    console.log('authToken undefined')
     return NextResponse.redirect(new URL('/es/login', request.url))
   }
+  return NextResponse.next()
 
-  try {
-    await jwtVerify(authToken.value, new TextEncoder().encode(process.env.TOKEN_SECRET))
-    return NextResponse.next()
-  } catch (e) {
-    return NextResponse.redirect(new URL('/es/login', request.url))
-  } 
+  // try {
+  //   await jwtVerify(authToken, new TextEncoder().encode(process.env.TOKEN_SECRET))
+  //   return NextResponse.next()
+  // } catch (e) {
+  //   return NextResponse.redirect(new URL('/es/login', request.url))
+  // }
 }
 
 export const config = {
