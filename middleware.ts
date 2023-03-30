e0f2ded6a8bf44fcbac65aa6f2dd51f3b8b10a0b
Joan Bosch <jbosch@adiquimica.com>
2023-03-30 17:12:48 +0200

    Disable verification

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-import { jwtVerify } from 'jose'
+// import { jwtVerify } from 'jose'
 
 export async function middleware (request: NextRequest) {
   console.log('middleware')
@@ -11,12 +11,12 @@ export async function middleware (request: NextRequest) {
     return NextResponse.redirect(new URL('/es/login', request.url))
   }
 
-  try {
-    await jwtVerify(authToken, new TextEncoder().encode(process.env.TOKEN_SECRET || ''))
-    return NextResponse.next()
-  } catch (e) {
-    return NextResponse.redirect(new URL('/es/login', request.url))
-  }
+  // try {
+  //   await jwtVerify(authToken, new TextEncoder().encode(process.env.TOKEN_SECRET || ''))
+  //   return NextResponse.next()
+  // } catch (e) {
+  //   return NextResponse.redirect(new URL('/es/login', request.url))
+  // }
 }
 
 export const config = {
