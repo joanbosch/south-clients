ad9e1538822b110049da4ecf61ae2e635933c7f4
Joan Bosch <jbosch@adiquimica.com>
2023-04-18 21:31:40 +0200

    Auto stash before rebase of "main"

--- components/register.tsx
+++ components/register.tsx
@@ -41,8 +41,12 @@ export default function RegisterComponent ({ params: { lang } } : { params: { la
               </h1>
               <form className='space-y-4 md:space-y-6' onSubmit={handleSubmit}>
                 <div>
-                  <label htmlFor='name' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Nombre y apellidos*</label>
-                  <input type='text' onChange={handleChange} name='name' required id='name' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='Antonio Pérez Rodríguez' />
+                  <label htmlFor='name' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Nombre*</label>
+                  <input type='text' onChange={handleChange} name='name' required id='name' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+                </div>
+                <div>
+                  <label htmlFor='surname' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Apellidos*</label>
+                  <input type='text' onChange={handleChange} name='surname' required id='surname' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                 </div>
                 <div>
                   <label htmlFor='phone' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Teléfono*</label>
@@ -50,31 +54,39 @@ export default function RegisterComponent ({ params: { lang } } : { params: { la
                 </div>
                 <div>
                   <label htmlFor='email' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Correo electrónico*</label>
-                  <input type='email' onChange={handleChange} name='email' required id='email' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='nombre@dominio.com' />
+                  <input type='email' onChange={handleChange} name='email' required id='email' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                 </div>
                 <div>
                   <label htmlFor='confirmEmail' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Confirma el correo electrónico*</label>
-                  <input type='email' onChange={handleChange} name='confirmEmail' required id='confirmEmail' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='nombre@dominio.com' />
+                  <input type='email' onChange={handleChange} name='confirmEmail' required id='confirmEmail' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                 </div>
                 <div>
                   <label htmlFor='password' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Contraseña*</label>
-                  <input type='password' onChange={handleChange} name='password' id='password' placeholder='••••••••' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+                  <input type='password' onChange={handleChange} name='password' id='password' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                 </div>
                 <div>
                   <label htmlFor='confirmPassword' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Repite la contraseña*</label>
-                  <input type='password' onChange={handleChange} name='confirmPassword' id='confirmPassword' placeholder='••••••••' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
-                </div>
-                <div>
-                  <label htmlFor='businessName' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Nombre de la empresa*</label>
-                  <input type='text' onChange={handleChange} name='businessName' required id='businessName' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
-                </div>
-                <div>
-                  <label htmlFor='NIF' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>NIF de la empresa*</label>
-                  <input type='text' onChange={handleChange} name='NIF' required id='NIF' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' placeholder='B071234567' />
+                  <input type='password' onChange={handleChange} name='confirmPassword' id='confirmPassword' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                 </div>
                 <div>
                   <label htmlFor='adress' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Dirección*</label>
                   <input type='text' onChange={handleChange} name='adress' required id='adress' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+                </div>
+                <div>
+                  <label htmlFor='adress_opt' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Datos adicionales dirección</label>
+                  <input type='text' onChange={handleChange} name='adress_opt' required id='adress_opt' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+                </div>
+                <div>
+                  <label htmlFor='country' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>País*</label>
+                  <select onChange={handleChange} name='country' required id='country' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500'>
+                    <option value=''>Selecciona una opción</option>
+                    <option value='España'>España</option>
+                    <option value='Portugal'>Portugal</option>
+                  </select>
+                </div>
+                <div>
+                  <label htmlFor='province' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Provincia*</label>
+                  <input type='text' onChange={handleChange} name='province' required id='province' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                 </div>
                 <div>
                   <label htmlFor='city' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Ciudad*</label>
@@ -84,6 +96,15 @@ export default function RegisterComponent ({ params: { lang } } : { params: { la
                   <label htmlFor='ZIP' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Código postal*</label>
                   <input type='text' onChange={handleChange} name='ZIP' required id='ZIP' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
                 </div>
+                <div>
+                  <label htmlFor='businessName' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>Nombre de la empresa*</label>
+                  <input type='text' onChange={handleChange} name='businessName' required id='businessName' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+                </div>
+                <div>
+                  <label htmlFor='NIF' className='block mb-2 text-sm font-medium text-gray-900 dark:text-black'>NIF de la empresa*</label>
+                  <input type='text' onChange={handleChange} name='NIF' required id='NIF' className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-southClearBrown2-700 dark:border-southDarkBrown2-600 dark:placeholder-gray-600 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+                </div>
+
                 <button type='submit' className='w-full text-white bg-southBrown hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-southBlue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-southBlue-600 dark:hover:bg-southBlue-700 dark:focus:ring-southBlue-800'>Registrarse</button>
               </form>
             </div>
